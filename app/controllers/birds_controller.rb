6334e66d533506b1daec33311df6ef1267b52a8e
Max <maxmwangikenya@gmail.com>
2023-06-22 12:30:26 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,25 +1,24 @@
+# BirdsController with wrap parameters disabled
 class BirdsController < ApplicationController
-
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
-
+  wrap_parameters format: []
 end
+class BirdWatcher
+  attr_accessor :name, :email, :bio, :favorite_species, :admin
+
+  def initialize(args)
+    @name = args[:name]
+    @email = args[:email]
+    @bio = args[:bio]
+    @favorite_species = args[:favorite_species]
+    @admin = args[:admin]
+  end
+end
+
+# Creating a new BirdWatcher object
+bird_watcher = BirdWatcher.new(
+  name: "Reggie",
+  email: "birdman5000@gmail.com",
+  favorite_species: "Crow",
+  bio: "Just a bird-loving guy",
+  admin: false
+)
